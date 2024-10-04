4616e15e8acbea8821fa76bc2a1e71084f03df27
MinatoDevel <sgminato69@gmail.com>
2024-10-04 09:03:41 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,73 @@
-import { ApifyClient } from 'apify-client';
+const express = require('express');
+const axios = require('axios');
 
-const client = new ApifyClient({
-  token: 'apify_api_hK2Pc8SGUgiFc23VS3HOPyLNYbDbFZ3XYF6r',
+const app = express();
+
+async function fetchDataWithRetry(username, retries = 2) {
+  try {
+    const options = {
+      method: 'POST',
+      url: 'https://tiktok-unauthorized-api-scraper-no-watermark-analytics-feed.p.rapidapi.com/api/search_full',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '3641222daamsh414c9dca6784a8ep1f9b60jsn92b32450ebbf',
+        'X-RapidAPI-Host': 'tiktok-unauthorized-api-scraper-no-watermark-analytics-feed.p.rapidapi.com'
+      },
+      data: {
+        username: `@${username}`,
+        amount_of_posts: 10
+      }
+    };
+
+    const response = await axios.request(options);
+    const posts = response.data.posts.map(post => {
+      const playLinks = post.play_links;
+      if (playLinks.length > 0) {
+        return playLinks[0]; 
+      } else {
+        return null;
+      }
+    });
+
+    return posts.filter(post => post !== null); 
+  } catch (error) {
+    console.error(error);
+    if (retries > 0) {
+      console.log(`Retrying... (${retries} retries left)`);
+      return fetchDataWithRetry(username, retries - 1);
+    } else {
+      throw new Error('Max retries exceeded');
+    }
+  }
+}
+app.get('/', (req, res) => {
+  res.send('tiktok');
+});
+app.get('/itachi', async (req, res) => {
+  try {
+    const username = req.query.username;
+    if (!username) {
+      return res.status(400).json({ error: 'Username parameter is missing' });
+    }
+
+    let posts = await fetchDataWithRetry(username);
+    if (!posts || posts.length === 0) {
+      posts = await fetchDataWithRetry(username);
+    }
+
+    const responseData = {
+      user: username,
+      posts: posts
+    };
+
+    res.json(responseData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-// Prepare Actor input
-const input = {
-  "startUrls": [
-    {
-      "url": "https://www.instagram.com/{username}/"
-    }
-  ],
-  "proxy": {
-    "useApifyProxy": true
-  },
-  "maxRequestRetries": 10
-};
-
-// Run the Actor and wait for it to finish
-const run = await client.actor("pocesar/download-instagram-video").call(input);
-
-// Fetch and print Actor results from the run's dataset (if there are any)
-console.log('Results from dataset');
-console.log(`Check your data here: https://console.apify.com/storage/datasets/${run.defaultDatasetId}`);
-const { items } = await client.dataset(run.defaultDatasetId).listItems();
-items.forEach((item) => {
-  console.dir(item);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
